2ad82fc3539a712389de380e3f3dc6c619a79d00
abhinav2697 <abhinav2697@gmail.com>
2024-02-15 22:54:21 +0530

    new files added

--- reactjscssgrid/src/App.js
+++ reactjscssgrid/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import Best from './components/Best/Best';
 function App() {
   return (
     <>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <Best/>
     
     </>
   );
---
+++ reactjscssgrid/src/components/Best/Best.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Best.css'
+
+const Best = () => {
+  return (
+      <div className='best'>
+          <h1>Find Best Rated Properties</h1>
+          <div>
+              <p><span className='bold'>All</span></p>
+              <p>Commercial</p>
+              <p>Residential</p>
+              <p>Agricultural</p>
+          </div>
+          </div>
+  )
+}
+
+export default Best
