ec32c7646f4816ff6ea668579c93af7312ad9a9d
abhinav2697 <abhinav2697@gmail.com>
2024-02-17 19:55:38 +0530

    new files added

--- reactjscssgrid/src/App.js
+++ reactjscssgrid/src/App.js
@@ -3,13 +3,15 @@ import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Best from './components/Best/Best';
 import Featured from './components/Featured/Featured';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <Best />
-      <Featured/>
+      <Featured />
+      <Footer/>
     
     </>
   );
